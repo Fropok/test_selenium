2a00f34556d05d25b52b5a34efa89c7647e99507
Fropok <fropike@gmail.com>
2025-06-20 03:25:14 +0300

    test_items.py editing time

--- Zadanie_3_6/test_items.py
+++ Zadanie_3_6/test_items.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 def test_add_to_cart_button(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     button_add_cart = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     assert button_add_cart.is_displayed(), 'ОШИБКА! Кнопка добавления в корзину не найдена'
 
